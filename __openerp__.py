bffcebe4e9ee8fa4432004e6baa2ec66c8e8b897
Tahany Sobhy <tota.sobhy91@gmail.com>
2015-04-02 13:50:20 +0200

    the second file

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name':'iti',
+    'version':'1.0',
+    'summary':'iti students data',
+    'description':"for saving students data",
+    'author':'iti',
+    'website':'http://www.iti.gov.eg',
+    'depends':['base'],
+    'data':['iti_view.xml'],
+    'installable':True,
+    'auto_install':False,
+
+}
