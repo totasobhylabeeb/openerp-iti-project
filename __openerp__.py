8bbe6e5c541f77ff37f86f443b8dae37aa59eb99
Tahany Sobhy <tota.sobhy91@gmail.com>
2015-04-02 14:36:57 +0200

    my second push openerp file

--- __openerp__.py
+++ __openerp__.py
@@ -1,13 +1,13 @@
 {
-    'name':'iti student',
+    'name':'project',
      'version':'1.0',
-	'summary':'iti students frist version',
+	'summary':'iti openerp project first version',
    'website':'http://wwww.iti.gov.eg',
     'description': 'Basic example of  iti module',
-'auther':'niven nabih',
+'auther':'niven-ahmed-aya=tahany-fatma',
     'depends': ['base','hr'],
 'data':[
-	'iti_view.xml',
+	'project_view.xml',
  	'security/iti_security.xml',
         'security/ir.model.access.csv',
 ],
