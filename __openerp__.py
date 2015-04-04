78e1bfacf619047c75e809a05a85db4707657dbd
Tahany Sobhy <tota.sobhy91@gmail.com>
2015-04-04 15:28:19 +0200

    ta3deel in openerp

--- __openerp__.py
+++ __openerp__.py
@@ -1,16 +1,16 @@
 {
-    'name':'project',
-     'version':'1.0',
-	'summary':'iti openerp project first version',
-   'website':'http://wwww.iti.gov.eg',
+    'name': 'project',
+    'version': '1.0',
+    'summary': 'iti openerp project first version',
+    'website': 'http://wwww.iti.gov.eg',
     'description': 'Basic example of  iti module',
-'auther':'niven-ahmed-aya-tahany-fatma',
-    'depends': ['base','hr','warehouse'],
-'data':[
-	'project_view.xml',
- 	'security/iti_security.xml',
+    'auther': 'niven-ahmed-aya-tahany-fatma',
+    'depends': ['base', 'hr', 'warehouse'],
+    'data': [
+        'project_view.xml',
+        'security/iti_security.xml',
         'security/ir.model.access.csv',
-],
-'installable':True,
-'auto_install':False,
+    ],
+    'installable': True,
+    'auto_install': False,
 }
