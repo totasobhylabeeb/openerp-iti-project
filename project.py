8db71db7eefb3322d281dc29e5f43218716231e7
Tahany Sobhy <tota.sobhy91@gmail.com>
2015-04-06 19:57:42 +0200

    add warehouse class

--- project.py
+++ project.py
@@ -1,25 +1,42 @@
 from openerp.osv import orm, fields
 
 
-
+class project_category(orm.Model):
+    _name = 'project.category'
+    _columns = {
+        'name': fields.char('Name', size=30),
+        'description': fields.text('Description'),
+        'code': fields.integer('Code'),
+        'subcategory_ids': fields.one2many('project.subcategory','category_id', 'Sub Category'),
+    }
 
 class project_subcategory(orm.Model):
     _name = 'project.subcategory'
     _columns = {
-        'name': fields.char('Name'),
-        'description': fields.char('Description'),
-        'code': fields.char('Code'),
-        'category_id': fields.many2one('project.category','Sub Category')
-	'subsubcategory_ids': fields.one2many('project.subsubcategory','subcategory_id','SubSub Category')
+        'name': fields.char('Name', size=30),
+        'description': fields.text('Description'),
+        'code': fields.integer('Code'),
+        'category_id': fields.many2one('project.category', 'Sub Category'),
+        'subsubcategory_ids': fields.one2many('project.subsubcategory', 'subcategory_id', 'SubSub Category')
 
     }
 
-class project_subsubCategory(orm.Model):
-    _name = 'project.subsubCategory'
-    _columns = {
-        'name': fields.char('Name'),
-        'description': fields.char('Description'),
-        'code': fields.char('Code'),
-        'subcategory_id': fields.many2one('project.subcategory','SubSub Category')
 
+class project_subsubcategory(orm.Model):
+    _name = 'project.subsubcategory'
+    _columns = {
+        'name': fields.char('Name', size=30),
+        'description': fields.text('Description'),
+        'code': fields.integer('Code'),
+        'subcategory_id': fields.many2one('project.subcategory', 'SubSub Category')
+
+    }
+class project_warehouse(orm.Model):
+    _name = 'project.warehouse'
+    _columns = {
+        'name':fields.char('Name', size=30, required=True),
+        'address':fields.char('Address', size=100),
+        #'keeper_id':fields.many2one('hr','Keeper',select=True),
+        #'manager_id':fields.many2one('hr','Manager',select=True),
+        #'supermanager_id':fields.many2one('hr','Supermanager',select=True),
     }
